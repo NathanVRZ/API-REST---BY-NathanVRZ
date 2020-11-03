99cb81d2a5dd6198b11af000870b565206453f51
Nathan Lino <nata.silva@bandtec.com.br>
2020-11-03 11:18:09 -0300

    cadastro sendo realizado

--- rest/routes/usuarios.js
+++ rest/routes/usuarios.js
@@ -2,9 +2,6 @@ const express = require('express');
 const router = express.Router();
 const mysql = require('../config').pool;
 const bcrypt = require('bcrypt');
-// const { INSERT } = require('sequelize/types/lib/query-types');
-// const { response } = require('express');
-
 
 router.get('/', (req, res, next) => {
     res.status(200).send({
@@ -18,15 +15,15 @@ router.post('/cadastro', (req, res, next) => {
         bcrypt.hash(req.body.senha, 10, (errBcrypt, hash) => {
             if (errBcrypt) { return res.status(500).send({ error: errBcrypt}) }
             conn.query(
-                `INSERT INTO Clientes (Email, senha, Loja, Rua, CNPJ, UnidadesAssinadas) VALUES (?,?,?,?,?,?)`,
-                [req.body.Email, hash, req.body.Loja, req.body.Rua, req.body.CNPJ, req.body.UnidadeAssinadas], 
+                `INSERT INTO usuario (Email, senha, Loja, Rua, CNPJ, UnidadesAssinadas) VALUES (?,?,?,?,?,?)`,
+                [req.body.Email, req.body.senha, req.body.Loja, req.body.Rua, req.body.CNPJ, req.body.UnidadesAssinadas], 
                 (error, results) => {
                     conn.release();
                     if (err) { return res.status(500).send({ error: error}) }
                     response = {
                         mensagem: 'usuario criado',
                         usuarioCriado: {
-                            idClientes: results.insertid,
+                            // idClientes: results.insertid,
                             Email: req.body.Email
                         }
                     }
